94f41354754110b05912004944f015706eb8020a
Jonas Vicente <jonas.vicente@rbmweb.com.br>
2023-05-03 12:41:29 -0300

    refactor: handlers

--- api/v1/routes/router.go
+++ api/v1/routes/router.go
@@ -1,15 +1,14 @@
 package routes
 
 import (
+	"restful-api/api/v1/handlers"
 	"restful-api/api/v1/middlewares"
-	"restful-api/internal/home"
-	"restful-api/internal/login"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetRoutes(router *gin.RouterGroup) {
-	router.GET("/", home.Home)
-	router.POST("/login", login.Login)
-	router.POST("/authenticated-test", middlewares.ValidateJWT, login.Login)
+	router.GET("/", handlers.Home)
+	router.POST("/login", handlers.Login)
+	router.POST("/authenticated-test", middlewares.ValidateJWT, handlers.Login)
 }
